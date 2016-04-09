26023b28783a71e0fafd231312066cce51b810bb
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-04-09 23:19:38 +0200

    begin qst2

---
+++ src/udp/ClientUDP.java
@@ -0,0 +1,70 @@
+package udp;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+/**
+ * Created by Cyril on 09/04/2016.
+ */
+public class ClientUDP {
+
+    protected DatagramSocket socket;
+    protected InetAddress address;
+    protected int port;
+    protected Set<String> possibilities;
+    protected static final int SIZEPACKET = 512;
+
+    public ClientUDP(String address, Integer port) throws SocketException, UnknownHostException {
+        this.openSocket();
+        this.address = InetAddress.getByName(address);
+        this.port = port;
+        this.initPossibilities();
+    }
+
+    public void openSocket() throws SocketException {
+        this.socket = new DatagramSocket();
+    }
+
+    public void closeSocket(){
+        this.socket.close();
+    }
+
+    public void initPossibilities(){
+        this.possibilities = new HashSet<>();
+        this.possibilities.add("affiche Salut");
+        this.possibilities.add("affiche Coucou");
+        this.possibilities.add("affiche Comment ca va ?");
+        this.possibilities.add("affiche :)");
+        this.possibilities.add("effacer");
+        this.possibilities.add("effacer");
+        this.possibilities.add("effacer");
+        this.possibilities.add("Salut");
+        this.possibilities.add("Autre");
+        this.possibilities.add("Error");
+        this.possibilities.add(":(");
+    }
+
+    public String randomMessage(){
+        List<String> asList = new ArrayList<>(this.possibilities);
+        Collections.shuffle(asList);
+        return asList.get(0);
+    }
+
+    public void sendRandomMessage() throws IOException {
+        String message = this.randomMessage();
+        this.socket.send(new DatagramPacket(message.getBytes(), message.length(), this.port));
+        this.manageAnswer();
+    }
+
+    public void manageAnswer() throws IOException {
+        DatagramPacket packet = new DatagramPacket(new byte[SIZEPACKET], SIZEPACKET);
+        this.socket.receive(packet);
+        byte[] array = packet.getData();
+        for(byte bit : array) {
+            System.out.print(Character.toString((char) bit));
+        }
+        System.out.println();
+    }
+
+}

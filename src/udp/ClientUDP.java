3d28c92efab328dc77873e5e0022b65368e083af
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-04-10 22:54:33 +0200

    Revert "Merged branch master into cf_TCP"
    This reverts commit 8c4937aaec767a22bca902ca487514de0ff00581.

--- src/udp/ClientUDP.java
+++ src/udp/ClientUDP.java
@@ -5,8 +5,7 @@ import java.net.*;
 import java.util.*;
 
 /**
- * I am a simple UDP client that send random messages to the server.
- * I contains a list of random message.
+ * Created by Cyril on 09/04/2016.
  */
 public class ClientUDP {
 
@@ -14,19 +13,8 @@ public class ClientUDP {
     protected DatagramSocket socket;
     protected InetAddress address;
     protected int port;
-
-    /**
-     * Some possibilities of message for the server.
-     */
     protected Set<String> possibilities;
 
-    /**
-     * I initialize the server. I open a socket, set the address, the port and the possible messages for the server.
-     * @param address The address of the server
-     * @param port The port of the server
-     * @throws SocketException Raised if there is a problem with the socket.
-     * @throws UnknownHostException Raised if there is a problem with the address of the server.
-     */
     public ClientUDP(String address, Integer port) throws SocketException, UnknownHostException {
         this.openSocket();
         System.out.println("Address: " + address);
@@ -36,26 +24,16 @@ public class ClientUDP {
         this.initPossibilities();
     }
 
-    /**
-     * I open an UDP Socket
-     * @throws SocketException Raised if there is a problem at the initialization.
-     */
     public void openSocket() throws SocketException {
         System.out.println("Open Socket");
         this.socket = new DatagramSocket();
     }
 
-    /**
-     * Close the socket
-     */
     public void closeSocket() {
         System.out.println("Close Socket");
         this.socket.close();
     }
 
-    /**
-     * Init the possibilities of message to send to the server
-     */
     public void initPossibilities() {
         this.possibilities = new HashSet<>();
         this.possibilities.add("afficher Salut");
@@ -71,10 +49,6 @@ public class ClientUDP {
         this.possibilities.add(":(");
     }
 
-    /**
-     * Select a random message.
-     * @return the selected message
-     */
     public String randomMessage() {
         System.out.println("Select random message");
         List<String> asList = new ArrayList<>(this.possibilities);
@@ -82,10 +56,6 @@ public class ClientUDP {
         return asList.get(0);
     }
 
-    /**
-     * Send a random message to the server
-     * @throws IOException Raised if there is a problem during the sending or reception of the message.
-     */
     public void sendRandomMessage() throws IOException {
         System.out.println("Create packet");
         String message = this.randomMessage();
@@ -96,10 +66,6 @@ public class ClientUDP {
         this.manageAnswer();
     }
 
-    /**
-     * After sending a message I wait for the answer.
-     * @throws IOException Raised if there is a problem during the reception.
-     */
     public void manageAnswer() throws IOException {
         System.out.println("Wait answer...");
         DatagramPacket packet = new DatagramPacket(new byte[SIZEPACKET], SIZEPACKET);

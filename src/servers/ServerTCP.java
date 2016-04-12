7f626911b96b0a916c1d64ad9d3276358e3d999e
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-04-12 22:41:05 +0200

    first try with threads

--- src/servers/ServerTCP.java
+++ src/servers/ServerTCP.java
@@ -1,11 +1,15 @@
 package servers;
 
+import gui.IHM;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.function.BiFunction;
 
 /**
  * I am a class that implement an TCP server.
@@ -13,7 +17,8 @@ import java.net.Socket;
 public class ServerTCP extends AbstractServer {
 
     protected ServerSocket socket;
-    protected Socket connectionSocket;
+
+    protected Socket connexionSocket;
 
 
     public ServerTCP() throws IOException {
@@ -28,45 +33,68 @@ public class ServerTCP extends AbstractServer {
     public void launch() throws IOException {
         while (!this.stop) {
             System.out.println("Open connection");
-            this.connectionSocket = this.socket.accept();
-            this.loopTreatment();
-
-            System.out.println("Close socket");
-            this.connectionSocket.close();
+            this.connexionSocket = this.socket.accept();
+            new Thread(new ClientHandler(connexionSocket, this.gui, this.commands)).start();
         }
-    }
-
-    public void loopTreatment() throws IOException {
-        System.out.println("Get data");
-        String[] receivedMessages = (new BufferedReader(new InputStreamReader(this.connectionSocket.getInputStream()))).readLine().split(" ", 2);
-
-        String result = this.executeAction(receivedMessages);
-
-        System.out.println("Send result");
-        (new DataOutputStream(connectionSocket.getOutputStream())).writeBytes(result + "\n");
-
-        //Loop
-        if (!this.stop) {
-            this.loopTreatment();
-        }
-    }
-
-    public String executeAction(String[] receivedMessages) {
-        System.out.println("Execute action");
-        String rest;
-        try {
-            rest = receivedMessages[1];
-        } catch (IndexOutOfBoundsException e) {
-            rest = "";
-        }
-
-        return commands.getOrDefault(receivedMessages[0], this.getErrorLambda()).apply(rest, this.gui);
     }
 
     public void stop() throws IOException {
         System.out.println("Stop server.");
         this.stop = true;
         this.socket.close();
+    }
+
+    public class ClientHandler implements Runnable {
+
+        protected Socket connectionSocket;
+
+        protected IHM gui;
+
+        protected HashMap<String, BiFunction<String, IHM, String>> commands;
+
+        public ClientHandler(Socket socket, IHM gui, HashMap<String, BiFunction<String, IHM, String>> commands) {
+            this.connectionSocket = socket;
+            this.gui = gui;
+            this.commands = commands;
+        }
+
+        public BiFunction<String, IHM, String> getErrorLambda() {
+            return (String order, IHM ihm) -> "ERREUR : Ordre inconnu";
+        }
+
+        public void run() {
+            System.out.println("Get data");
+            String[] receivedMessages = new String[0];
+            try {
+                receivedMessages = (new BufferedReader(new InputStreamReader(this.connectionSocket.getInputStream()))).readLine().split(" ", 2);
+            } catch (IOException e) {
+                e.printStackTrace(); //TODO
+            }
+
+            String result = this.executeAction(receivedMessages);
+
+            System.out.println("Send result");
+            try {
+                (new DataOutputStream(connectionSocket.getOutputStream())).writeBytes(result + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();//TODO
+            }
+
+            this.run();
+        }
+
+        public String executeAction(String[] receivedMessages) {
+            System.out.println("Execute action");
+            String rest;
+            try {
+                rest = receivedMessages[1];
+            } catch (IndexOutOfBoundsException e) {
+                rest = "";
+            }
+
+            return commands.getOrDefault(receivedMessages[0], this.getErrorLambda()).apply(rest, this.gui);
+        }
+
     }
 
 }
